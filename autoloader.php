baab601a913b4dc131c645e75950506ecec781f9
Alexey Jukov <alexismaster@yandex.ru>
2015-07-24 09:43:46 +0300

    Рефакторинг автозагрузчика

--- autoloader.php
+++ autoloader.php
@@ -14,11 +14,11 @@ Twig_Autoloader::register();
 
 // Автозагрузчик классов приложения
 spl_autoload_register(function ($className) {
-  $path = preg_replace('/\\\/', '/', $className);
-  $path = preg_replace('/^Nirvana/', 'framework', $path);             // Классы фреймворка
-  $path = preg_replace('/^SRC/', 'src', $path);                       // Классы приложения
+  $path = str_replace('\\', '/', $className);
+  $path = str_replace('Nirvana/', 'framework/', $path);               // Классы фреймворка
+  $path = str_replace('SRC/', 'src/', $path);                         // Классы приложения
   $path = preg_replace('/\\/([A-z]+Module)/', '/modules/$1', $path);  // Классы модулей приложения
   $path = $path . '.php';
 
-  if (is_file($path)) require_once $path;
+  require_once $path;
 });
